26c50c069bae27f40e6475d2f2d4f6d0320c325d
AnshumanGo <anshuman.g.cs@gmail.com>
2021-09-06 16:16:11 +0530

    spring boot with embedded h2 database

---
+++ _02spring-boot-with-embedded-h2-database/src/main/java/com/example/_02springbootwithembeddedh2database/Application.java
@@ -0,0 +1,13 @@
+package com.example._02springbootwithembeddedh2database;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+}
